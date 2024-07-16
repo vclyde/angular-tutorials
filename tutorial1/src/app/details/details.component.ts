cc1791bcb972c9d81ca1bc8331586288fd70dde6
vclyde <clydevelasquez@outlook.com>
2024-07-16 23:09:34 +0800

    Fix image errors

--- tutorial1/src/app/details/details.component.ts
+++ tutorial1/src/app/details/details.component.ts
@@ -61,7 +61,7 @@ export class DetailsComponent {
   });
 
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
     this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
       this.housingLocation = housingLocation;
     });
--- tutorial1/src/app/housing-location/housing-location.component.ts
+++ tutorial1/src/app/housing-location/housing-location.component.ts
@@ -7,11 +7,11 @@ import { RouterModule } from '@angular/router';
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  template:`
+  template: `
   <section class="listing">
     <img
       class="listing-photo" 
-      [src]=housingLocation.photo 
+      [src]="housingLocation.photo" 
       alt="Exterior photo of {{ housingLocation.name }}" 
       crossorigin >
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
